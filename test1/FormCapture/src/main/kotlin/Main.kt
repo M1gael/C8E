c368b5f9fedeec5c068a14465417af8f9f97bc4a
M1gael <smit.migael.johannes@gmail.com>
2025-12-02 00:20:13 +0200

    Post value extraction and data parsing complete

--- test1/FormCapture/src/main/kotlin/Main.kt
+++ test1/FormCapture/src/main/kotlin/Main.kt
@@ -12,17 +12,29 @@ fun main() {
     //default route
     server.createContext("/") { exchange ->
 
-        //read data from our index and store bytes
-        val inputStream = object{}.javaClass.getResourceAsStream("/index.html")
-        val filebytes = inputStream.readBytes()
-
         if (exchange.requestMethod == "GET") {
+            //read data from our index and store bytes
+            val inputStream = object{}.javaClass.getResourceAsStream("/index.html")
+            val fileBytes = inputStream.readBytes()
+
             exchange.responseHeaders.add("Content-Type" , "text/html")
-            exchange.sendResponseHeaders(200 , filebytes.size.toLong())
-            exchange.responseBody.use { os -> os.write(filebytes)}
+            exchange.sendResponseHeaders(200 , fileBytes.size.toLong())
+            exchange.responseBody.use { os -> os.write(fileBytes)}
+            println("File Bytes :" + fileBytes)
         }
         else if (exchange.requestMethod == "POST"){
-            //mongo
+            //stream data to formData , segment into formparts
+            val formData = exchange.requestBody.reader().readText()
+            println("Caputred Form Data :" + formData)
+            val formParts = formData.split('&')
+
+            //manual form parser
+            val params = mutableMapOf<String , String>()
+            for (part in formParts){
+                val keyValue = part.split('=')
+                params[keyValue[0]]=keyValue[1]
+            }
+
         }
         else{
             //error handle?
