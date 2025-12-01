fbaa0ba9b6d856d6adf4b59dd2bfd9cb43a24ee6
M1gael <smit.migael.johannes@gmail.com>
2025-12-01 21:39:18 +0200

    GET form working condition , pre POST

--- test1/FormCapture/src/main/kotlin/Main.kt
+++ test1/FormCapture/src/main/kotlin/Main.kt
@@ -11,12 +11,26 @@ fun main() {
 
     //default route
     server.createContext("/") { exchange ->
-        val response = "Do your worst"
-        exchange.sendResponseHeaders(200 , response.length.toLong())
-        exchange.responseBody.use { os -> os.write(response.toByteArray())}
+
+        //read data from our index and store bytes
+        val inputStream = object{}.javaClass.getResourceAsStream("/index.html")
+        val filebytes = inputStream.readBytes()
+
+        if (exchange.requestMethod == "GET") {
+            exchange.responseHeaders.add("Content-Type" , "text/html")
+            exchange.sendResponseHeaders(200 , filebytes.size.toLong())
+            exchange.responseBody.use { os -> os.write(filebytes)}
+        }
+        else if (exchange.requestMethod == "POST"){
+            //mongo
+        }
+        else{
+            //error handle?
+        }
     }
 
     //start
     println("Started server on port 8000")
     server.start()
 }
+
