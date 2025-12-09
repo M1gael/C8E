cd5dc74b507e2157641eee8ba0a2d2e3be636a14
M1gael <smit.migael.johannes@gmail.com>
2025-12-09 04:51:18 +0200

    staged , decided on approach , keeping string based x count loops to fill. HashSet Auto deters duplicates.

---
+++ test2/ArraysAndFileHandling/src/main/kotlin/Main.kt
@@ -0,0 +1,64 @@
+package org.example
+
+import java.util.Scanner
+import kotlin.random.Random
+
+fun main() {
+
+    //predefine 2 arrays
+    val names = arrayOf(
+        "Bruce", "Walter", "Oliver", "Elijah", "James", "William", "Benjamin", "Lucas", "Henry", "Theodore",
+        "Emma", "Olivia", "Ava", "Charlotte", "Sophia", "Amelia", "Isabella", "Mia", "Evelyn", "Harper"
+    )
+    val surnames = arrayOf(
+        "Wayne", "White", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
+        "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin"
+    )
+    //map all 400 name surnames and chose there?
+
+    val scanner = Scanner(System.`in`)
+
+    //program loop
+    var again = true
+    while (again){
+        print("Provide the amount of records to generate : ")
+        val recordCount = scanner.nextInt()
+
+        val records = HashSet<String>()
+
+        while (records.size < recordCount){
+            val name = names.random()
+            val surname = surnames.random()
+            val birthYear = Random.nextInt(1950 , 2026)
+            val birthMonth = Random.nextInt(1 , 13)
+            //deterministic birthdays
+            var birthDay = 0
+                if ( ((birthYear%4==0) && (birthYear%100!=0)) || (birthYear%400==0) ){
+                    when(birthMonth){
+                        2 -> birthDay = Random.nextInt(1,30)
+                        4,6,9,11 -> birthDay = Random.nextInt(1,31)
+                        else -> birthDay = Random.nextInt(1,32)
+                    }
+                }else {
+                    when(birthMonth){
+                        2 -> birthDay = Random.nextInt(1,29)
+                        4,6,9,11 -> birthDay = Random.nextInt(1,31)
+                        else -> birthDay = Random.nextInt(1,32)
+                    }
+                }
+            val birthDate = String.format("%02d/%02d/%d", birthDay, birthMonth, birthYear)
+            TODO()//add AGE and initial
+            //start building the entry list in mem
+            records.add( records.size.toString() + name + " " + surname + " " + birthDate)
+        }
+
+        println("\n\n========== COMPLETED ==========\n")
+        print("Would you like to do another entry? (Y?)")
+        if (scanner.next().compareTo("Y") != 0)
+            again = false
+
+    }
+
+    scanner.close()
+
+}
